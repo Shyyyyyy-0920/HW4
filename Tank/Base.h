94fc2609d8fcb6265bca73d206ee85e4a4d0e90a
Shyyyyyy <3410434256@qq.com>
2025-04-01 01:20:39 +0800

    暂时完成车子所有方向的绘制

---
+++ Tank/Base.h
@@ -0,0 +1,150 @@
+#pragma once
+
+/// \file
+/// \brief This file contains the most basic utilities.
+// Rand函数的作用是随机生成一个整数，范围在[0,n)之间，用法是Rand(n).
+// Rand01函数的作用是随机生成一个double类型的数，范围在[0,1)之间，用法是Rand01().
+// vec结构体是一个二维向量，包含两个整数x和y，提供了一些常用的操作函数，如Eq, Neq, Add, Sub, Mul等.
+// Eq函数用于判断两个Vec是否相等，Neq函数用于判断两个Vec是否不相等.
+// Add函数用于将两个Vec相加，Sub函数用于将两个Vec相减，Mul函数用于将Vec与一个整数相乘.
+// RandVec函数的作用是随机生成一个Vec类型的数，范围在[0,v.x) * [0,v.y)，用法是RandVec(v).
+// SleepMs函数的作用是让当前线程休眠指定的毫秒数，参数time为毫秒数.
+// Daze函数的作用是让当前线程暂停执行，具体实现根据编译器和平台不同而不同.
+#if defined(__MINGW64_VERSION_MAJOR) && __MINGW64_VERSION_MAJOR < 7
+  #error Your MinGW version is too old. Please update it to a newer version.
+#endif
+
+#include "detail/Macros.h"
+
+#include <limits.h>
+#include <math.h>
+#include <stddef.h>
+#include <stdint.h>
+#if _WIN32
+  #include <Synchapi.h>
+#else
+  #include <unistd.h>
+#endif
+
+/// \brief An alias of `unsigned int`.
+typedef unsigned uint;
+
+typedef int8_t int8;
+typedef uint8_t uint8;
+
+typedef int64_t int64;
+typedef uint64_t uint64;
+
+/// \brief Randomly generate an `int` in `[0, n)`.
+int Rand(int n) {
+  TK_ASSERT(n <= RAND_MAX,
+            "`n` is too large to be handled by `Rand`, please implement your own random number generator");
+  return rand() % n;
+}
+
+/// \brief Randomly generate a `double` in `[0, 1)`.
+double Rand01(void) {
+  return (double)rand() / ((double)RAND_MAX + 1.0);
+}
+
+//
+//
+//
+/// \brief A 2D vector which contains only `int`s.
+typedef struct {
+  int x, y;
+} Vec;
+
+bool Eq(Vec a, Vec b) {
+  return a.x == b.x && a.y == b.y;
+}
+
+bool Neq(Vec a, Vec b) {
+  return !Eq(a, b);
+}
+
+Vec Add(Vec a, Vec b) {
+  return (Vec){a.x + b.x, a.y + b.y};
+}
+
+Vec Sub(Vec a, Vec b) {
+  return (Vec){a.x - b.x, a.y - b.y};
+}
+
+Vec Mul(Vec a, int b) {
+  return (Vec){a.x * b, a.y * b};
+}
+
+/// \brief Randomly generate a `Vec` in `[0, v.x) * [0, v.y)`.
+Vec RandVec(Vec v) {
+  TK_ASSERT(v.x <= RAND_MAX && v.y <= RAND_MAX,
+            "`v` is too large to be handled by `RandVec`, please implement your own random number generator");
+  return (Vec){rand() % v.x, rand() % v.y};
+}
+
+//
+//
+//
+typedef const char *Color;
+#define TK_RUNTIME_COLOR "%s"
+
+#define TK_NORMAL        "0"
+#define TK_BLACK         "30"
+#define TK_RED           "31"
+#define TK_GREEN         "32"
+#define TK_YELLOW        "33"
+#define TK_BLUE          "34"
+#define TK_MAGENTA       "35"
+#define TK_CYAN          "36"
+#define TK_WHITE         "37"
+#define TK_BRIGHT_YELLOW "93"
+#define TK_BRIGHT_BLUE   "94"
+
+#define __TK_SET_COLOR(color) "\033[" color "m"
+
+#define __TK_TEXT1(text)        text
+#define __TK_TEXT2(text, color) __TK_SET_COLOR(color) text __TK_SET_COLOR(TK_NORMAL)
+
+/// \brief Get an optionally colored text.
+///
+/// \example ```c
+/// // Colors can be specified at compile-time, by using `TK_RED`, etc.
+/// printf(TK_TEXT("Hello World!\n"));
+/// printf(TK_TEXT("Hello World!\n", TK_RED));
+/// printf(TK_TEXT("Hello World%c\n", TK_YELLOW), '!');
+/// printf(TK_TEXT("Hello World%s\n", TK_GREEN), "!");
+///
+/// // Colors can also be specified at runtime, by using `Color` and `TK_RUNTIME_COLOR`.
+/// Color color = TK_BLUE;
+/// printf(TK_TEXT("Hello World%c\n", TK_RUNTIME_COLOR), color, '!');
+/// ```
+#define TK_TEXT(...) __TK_EXPAND(__TK_EXPAND(TK_CONCAT(__TK_TEXT, TK_NUM_OF(__VA_ARGS__)))(__VA_ARGS__))
+
+//
+//
+//
+/// \brief Suspends the execution of the current thread until
+/// the time-out interval (in milliseconds) elapses.
+///
+/// \example ```c
+/// SleepMs(1000); // Sleep for 1000 milliseconds.
+/// ```
+void SleepMs(uint time) {
+#ifdef _WIN32
+  Sleep(time);
+#else
+  usleep(time * 1000);
+#endif // _WIN32
+}
+
+/// \brief Suspends the execution of the current thread for
+/// only several nanoseconds.
+void Daze(void) {
+#if TK_ICC || TK_MSVC
+  _mm_pause();
+#elif __arm__ || __aarch64__
+  __builtin_arm_yield();
+#else
+  __builtin_ia32_pause();
+#endif
+}
---
+++ Tank/Game.h
@@ -0,0 +1,217 @@
+#pragma once
+
+/// \file
+/// \brief This file contains the game lifecycle and logics.
+/// There are 5 important functions:
+/// `GameInit`, `GameInput`, `GameUpdate`, `GameTerminate`, and
+/// the most important: `GameLifecycle`.
+/// Please read the corresponding comments to understand their relationships.
+
+//
+//
+//
+//
+//
+#include "Config.h"
+#include "Renderer.h"
+
+#include <time.h>
+
+typedef struct {
+  char keyHit; // The keyboard key hit by the player at this frame.
+} Game;
+
+// The game singleton.
+static Game game;
+
+// The keyboard key "ESC".
+static const char keyESC = '\033';
+
+//
+//
+//
+/// \brief Configure the scene (See `Scene.h`) with `config` (See `Config.h`), and
+/// perform initializations including:
+/// 1. Terminal setup.
+/// 2. Memory allocations.
+/// 3. Map and object generations.
+/// 4. Rendering of the initialized scene.
+///
+/// \note This function should be called at the very beginning of `GameLifecycle`.
+void GameInit(void) {
+  // Setup terminal.
+  TermSetupGameEnvironment();
+  TermClearScreen();
+
+  // Configure scene.
+  map.size = config.mapSize;
+  int nEnemies = config.nEnemies;
+  int nSolids = config.nSolids;
+  int nWalls = config.nWalls;
+
+  // 初始化场景.
+  RegInit(regTank);
+  RegInit(regBullet);
+
+  map.flags = (Flag *)malloc(sizeof(Flag) * map.size.x * map.size.y);
+  for (int y = 0; y < map.size.y; ++y)
+    for (int x = 0; x < map.size.x; ++x) {
+      Vec pos = {x, y};
+
+      Flag flag = eFlagNone;
+      if (x == 0 || y == 0 || x == map.size.x - 1 || y == map.size.y - 1) // 确立边界为墙壁，无法穿过无法破坏
+        flag = eFlagSolid;
+
+      map.flags[Idx(pos)] = flag; // 其他地方都是空地
+    }
+  for (int i = 0; i < nWalls; i++) { // 随机添加墙壁，数量由config.nWalls决定
+    Vec pos = RandVec(map.size);
+    if (map.flags[Idx(pos)] == eFlagNone) { // 只能在空地上添加墙壁
+      map.flags[Idx(pos)] = eFlagWall;      // 设置为墙壁
+    }
+  }
+  for (int i = 0; i < nSolids; i++) { // 随机添加坚固物体，数量由config.nSolids决定
+    Vec pos = RandVec(map.size);
+    if (map.flags[Idx(pos)] == eFlagNone) { // 只能在空地上添加坚固物体
+      map.flags[Idx(pos)] = eFlagSolid;     // 设置为坚固物体
+    }
+  }
+  { // 此为玩家操控的tank
+    Tank *tank = RegNew(regTank);
+    tank->pos = (Vec){6, 2}; // 玩家坦克的初始位置，(x, y) 代表第x列第y行
+    tank->dir = eDirPO;      // 初始运动方向
+    tank->color = TK_GREEN;  // 玩家坦克的颜色，绿色
+    tank->isPlayer = true;   // 是否为玩家坦克，true代表玩家坦克
+  }
+
+  // Initialize renderer.
+  renderer.csPrev = (char *)malloc(sizeof(char) * map.size.x * map.size.y);
+  renderer.colorsPrev = (Color *)malloc(sizeof(Color) * map.size.x * map.size.y);
+  renderer.cs = (char *)malloc(sizeof(char) * map.size.x * map.size.y);
+  renderer.colors = (Color *)malloc(sizeof(Color) * map.size.x * map.size.y);
+
+  for (int i = 0; i < map.size.x * map.size.y; ++i) {
+    renderer.csPrev[i] = renderer.cs[i] = ' ';
+    renderer.colorsPrev[i] = renderer.colors[i] = TK_NORMAL;
+  }
+
+  // Render scene.
+  for (int y = 0; y < map.size.y; ++y)
+    for (int x = 0; x < map.size.x; ++x) {
+      Vec pos = {x, y};
+      RdrPutChar(pos, map.flags[Idx(pos)], TK_AUTO_COLOR);
+    }
+  RdrRender();
+  RdrFlush();
+}
+
+//
+//
+//
+/// \brief Read input from the player.
+///
+/// \note This function should be called in the loop of `GameLifecycle` before `GameUpdate`.
+void GameInput(void) {
+  game.keyHit = kbhit_t() ? (char)getch_t() : '\0';
+}
+
+//
+//
+//
+/// \brief Perform all tasks required for a frame update, including:
+/// 1. Game logics of `Tank`s, `Bullet`s, etc.
+/// 2. Rerendering all objects in the scene.
+///
+/// \note This function should be called in the loop of `GameLifecycle` after `GameInput`.
+void GameUpdate(void) {
+  RdrClear();
+
+  // TODO: You may need to delete or add codes here.
+  // 这里目前是更新tank的移动方向
+  for (RegIterator it = RegBegin(regTank); it != RegEnd(regTank); it = RegNext(it)) {
+    Tank *tank = RegEntry(regTank, it);
+    // printf("%d\n", (int)RegSize(regTank)); // 输出当前tank的数量，便于调试
+    //  printf("x:%d y:%d", tank->pos.x, tank->pos.y);
+    if (game.keyHit == 'w' && tank->isPlayer) { // 玩家按w键向上移动
+      tank->dir = eDirOP;                       // 朝上
+      if (tank->pos.y < map.size.y - 3)
+        ++tank->pos.y;
+      if (map.flags[Idx(tank->pos)] != ' ')
+        --tank->pos.y;
+    } else if (game.keyHit == 's' && tank->isPlayer) { // 玩家按s键向下移动
+      tank->dir = eDirON;                              // 朝下
+      if (tank->pos.y > 2)
+        --tank->pos.y;
+      if (map.flags[Idx(tank->pos)] != ' ')
+        ++tank->pos.y;
+    } else if (game.keyHit == 'a' && tank->isPlayer) { // 玩家按a键向左移动
+      tank->dir = eDirNO;                              // 朝左
+      if (tank->pos.x > 2)
+        --tank->pos.x;
+      if (map.flags[Idx(tank->pos)] != ' ')
+        ++tank->pos.x;
+    } else if (game.keyHit == 'd' && tank->isPlayer) { // 玩家按d键向右移动
+      tank->dir = eDirPO;                              // 朝右
+      if (tank->pos.x < map.size.x - 3)
+        ++tank->pos.x;
+      if (map.flags[Idx(tank->pos)] != ' ')
+        --tank->pos.x;
+    }
+  }
+
+  RdrRender();
+  RdrFlush();
+}
+
+//
+//
+//
+/// \brief Terminate the game and free all the resources.
+///
+/// \note This function should be called at the very end of `GameLifecycle`.
+void GameTerminate(void) {
+  while (RegSize(regTank) > 0)
+    RegDelete(RegEntry(regTank, RegBegin(regTank)));
+
+  while (RegSize(regBullet) > 0)
+    RegDelete(RegEntry(regBullet, RegBegin(regBullet)));
+
+  free(map.flags);
+
+  free(renderer.csPrev);
+  free(renderer.colorsPrev);
+  free(renderer.cs);
+  free(renderer.colors);
+
+  TermClearScreen();
+}
+
+//
+//
+//
+/// \brief Lifecycle of the game, defined by calling the 4 important functions:
+/// `GameInit`, `GameInput`, `GameUpdate`, and `GameTerminate`.
+///
+/// \note This function should be called by `main`.
+void GameLifecycle(void) {
+  GameInit(); // 游戏初始化操作，必须要进行
+
+  double frameTime = (double)1000 / (double)config.fps; // 帧率，每帧的时间间隔，单位为毫秒
+  clock_t frameBegin = clock();                         // 记录程序开始运行时间
+
+  while (true) {
+    // printf(TK_TEXT("Hello World%c\n", TK_YELLOW), '!');
+
+    GameInput(); // 玩家输入
+    if (game.keyHit == keyESC)
+      break; // 玩家按Esc可以直接退出
+
+    GameUpdate(); // 这里实现逻辑上更新
+
+    while (((double)(clock() - frameBegin) / CLOCKS_PER_SEC) * 1000.0 < frameTime - 0.5)
+      Daze();
+    frameBegin = clock();
+  }
+
+  GameTerminate();
+}
